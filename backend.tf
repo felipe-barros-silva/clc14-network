e2d915784f9b6f2eaa13a9b474a65a56f5545249
EC2 Default User <ec2-user@ip-172-31-1-254.ec2.internal>
2025-11-25 22:59:30 +0000

    Ajustando configuracoes

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "clc14-barros-terraform"
+    bucket = "barros-terraform-clc14"
     key    = "network/terraform.tfstate"
     region = "us-east-1"
     use_lockfile = true
